ce890f4042b806d2a0f0b63698d237fe184442de
NancyGomez <nagomez@csumb.edu>
2018-04-01 20:34:43 -0700

    Added new viewcontroller and embedded nav controller

---
+++ tinder/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  tinder
+//
+//  Created by Nancy Gomez on 4/1/18.
+//  Copyright © 2018 Carlos Huizar-Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
